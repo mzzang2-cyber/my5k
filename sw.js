d4f154fc38b6e0d14a9aaa34f884ec5d85a19966
mzzang2-cyber <mzzang2@gmail.com>
2026-09-07 16:18:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my5k-v1';
+const CACHE='my5k-v2';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
